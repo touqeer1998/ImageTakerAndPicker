bb34140953620273ff798b4bf9bc419905d45723
Touqeer Ahmed <touqeer.greyneon@gmail.com>
2024-12-18 17:55:41 +0500

    initial commit

---
+++ app/src/main/java/com/example/imagetakerandpicker/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.imagetakerandpicker
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    private val CAMERA_CAPTURE_REQUEST_CODE = 101
+    private val GALLERY_PICK_REQUEST_CODE = 103
+
+    private lateinit var currentPhotoPath: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val attach: ImageView = findViewById(R.id.ivAttachImage)
+        val camera: ImageView = findViewById(R.id.ivCamera)
+
+        attach.setOnClickListener {
+            openGallery()
+        }
+        camera.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val photoFile: File? = try {
+            createImageFile()
+        } catch (ex: IOException) {
+            null
+        }
+
+        photoFile?.let {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this, "${applicationContext.packageName}.provider", it
+            )
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            startActivityForResult(intent, CAMERA_CAPTURE_REQUEST_CODE)
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*" // Only allow image selection
+        startActivityForResult(intent, GALLERY_PICK_REQUEST_CODE)
+    }
+
+    private fun getRealPathFromUri(uri: Uri): String? {
+        var filePath: String? = null
+        val cursor = contentResolver.query(uri, arrayOf(MediaStore.Images.Media.DATA), null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                filePath = it.getString(columnIndex)
+            }
+        }
+        return filePath
+    }
+
+    private fun savePhotoToDatabase(currentPhotoPath: String) {
+        Log.i("MainActivity", "currentPhotoPath $currentPhotoPath")
+        val image: ImageView = findViewById(R.id.ivImage)
+        Glide.with(this).load(currentPhotoPath).into(image)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_CAPTURE_REQUEST_CODE -> {
+                    savePhotoToDatabase(currentPhotoPath)
+                }
+                GALLERY_PICK_REQUEST_CODE -> {
+                    data?.data?.let { uri ->
+                        savePhotoToDatabase(uri.toString())
+                        getRealPathFromUri(uri)?.also { path ->
+                            savePhotoToDatabase(path)
+                        }
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
